5daaceb160ee6ea1504818538bfd2a2b811f7328
mooz <stillpedant@gmail.com>
2011-07-11 21:15:09 +0900

    Simple script.rb

--- script.rb
+++ script.rb
@@ -4,10 +4,30 @@ require "rios-proxy"
 
 proxy = Rios::Proxy.new
 
-open("/tmp/script", "w") do |file|
-  proxy.output do |s|
-    file.write(s)
-  end
+file_path = "/tmp/script"
 
-  proxy.listen()
+file = open(file_path, "w")
+
+puts <<EOS
+============================================================
+Begin recording script into #{file_path}
+============================================================
+EOS
+
+proxy.on_output do |s|
+  s = s.gsub(/a/, "!")
+  file.write(s)
+  s
 end
+
+proxy.on_finish do ||
+  file.close
+
+  puts <<EOS
+============================================================
+Recorded script into #{file_path}
+============================================================
+EOS
+end
+
+proxy.listen
